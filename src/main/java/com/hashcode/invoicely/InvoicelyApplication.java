6ae94cc1d366c3d2aa0ed26be08fe3afa275421e
Sahu, Gaurav <gsahu@ptc.com>
2025-06-16 18:46:08 +0530

    Latest Repo

---
+++ src/main/java/com/hashcode/invoicely/InvoicelyApplication.java
@@ -0,0 +1,13 @@
+package com.hashcode.invoicely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicelyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoicelyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hashcode/invoicely/InvoicelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashcode.invoicely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicelyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
